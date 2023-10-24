632b484cb52f28dcbacc6b3a6ea095fcf5d3d6d8
WoodyTheMaan <buzz72woody@gmail.com>
2023-10-24 10:27:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sammie_s_____P_A_R_K_I_N_G____GARAGE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firsthour, lastHour;
+            Console.WriteLine("Welcome to Sammie's Parking Garage!");
+            Console.WriteLine("Please type the time you parked your car.");
+            firsthour = Convert.ToInt32(Console.ReadLine());
+            if (firsthour == 0) { }
+
+
+
+       
+
+            
+        }
+    }
+}
