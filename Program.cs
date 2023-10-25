eddc7ce8c10d253a6415afb768f0de7584bf7ccf
WoodyTheMaan <buzz72woody@gmail.com>
2023-10-25 10:35:09 -0400

    Finished program

--- Program.cs
+++ Program.cs
@@ -4,16 +4,62 @@
     {
         static void Main(string[] args)
         {
-            int firsthour, lastHour;
+            int anountOfMinutes, moneyPaid = 0;
             Console.WriteLine("Welcome to Sammie's Parking Garage!");
-            Console.WriteLine("Please type the time you parked your car.");
-            firsthour = Convert.ToInt32(Console.ReadLine());
-            if (firsthour == 0) { }
+            Console.WriteLine("Please type the amount of time in minutes you've had your your car parked.");
+            anountOfMinutes = Convert.ToInt32(Console.ReadLine());
+            if (anountOfMinutes <= 60)
+            {
+                moneyPaid = 4;
+                
+            }
+            else if (anountOfMinutes <= 120)
+            {
+                moneyPaid = 4 + 2;
+                
+            }
+            else if (anountOfMinutes <= 180)
+            {
+                moneyPaid = 4 + 2 + 2;
+            }
+            else if (anountOfMinutes <= 240)
+            {
+                moneyPaid += 4 + 2 + 2 + 2;
+            }
+            else if (anountOfMinutes <= 300)
+            {
+                moneyPaid = 4 + 2 + 2 + 2 + 2;
+            }
+            else if (anountOfMinutes <= 360)
+            {
+                moneyPaid = 4 + 2 + 2 + 2 + 2 + 2;
+            }
+            else if (anountOfMinutes <= 420)
+            {
+                moneyPaid = 4 + 2 + 2 + 2 + 2 + 2 + 2;
+            }
+            else if (anountOfMinutes <= 480)
+            {
+                moneyPaid = 4 + 2 + 2 + 2 + 2 + 2 + 2 + 2;
+            }
+            else
+            {
+                moneyPaid = 4 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2;
+            }
+            Console.WriteLine("SAMMIE'S PARKING GARAGE INC.");
+            Console.WriteLine("=========================================================================================================");
+            Console.WriteLine($"You must pay ${moneyPaid} for parking");
+            Console.WriteLine("YOU WILL BE CHARGED EXTRA IF YOU DO NOT PAY WITHIN 24 HOURS (Amount of money charged varys on what day)");
+            Console.WriteLine("USE CODE: 11829188361738182 TO PAY BILL AT OUR WEBSITE");
+            Console.WriteLine("=========================================================================================================");
+            Console.WriteLine("Thanks for using Sammie's Parking Garage!");
 
 
 
-       
 
+
+
+            Console.ReadLine();
             
         }
     }
